ba671b217fb8b1f0bda5b2a67a25a5470cc9d18c
Rushikeshrandive97 <rushirandive98@gmail.com>
2026-01-03 10:47:51 +0530

    components

---
+++ components/Footer.tsx
@@ -0,0 +1,6 @@
+export default async function Footer() {
+    return (
+        <div>footer</div>
+    )
+    
+}
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default async function Header() {
+    return <header>
+        <div className="container">
+            {/*logo*/}
+            <link href={"/" }></link>
+        </div>
+    </header>
+    
+}
